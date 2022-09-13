7242141a6ebc9a3708d01f36ee89dec74928f3d3
xCrypt0r <legacy5877@gmail.com>
2022-09-14 00:59:39 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ const NETMARBLE_API_URL = 'https://nbill.netmarble.net/Cash/AutoPaymentPremium/A
 
 export const USERNAME_PLACEHOLDER = 'id를 입력하세요' as const;
 export const USERNAME_NOT_APPLICABLE = 'N/A' as const;
-export const REGEX_TRUTHY_PREFIX = /true\//;
+export const REGEX_TRUTHY_PREFIX = /^true\//;
 export const ENCODING_EUCKR = 'euc-kr' as const;
 export const EMPTY_STRING = '' as const;
 export const LOGO_ALTERNATIVE_TEXT = 'logo' as const;
