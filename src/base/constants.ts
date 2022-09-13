91c0395207b6783688add40a8ce868cb62627a86
xCrypt0r <legacy5877@gmail.com>
2022-09-14 07:25:31 +0900

    Update constants.ts

--- src/base/constants.ts
+++ src/base/constants.ts
@@ -1,3 +1,8 @@
+import type {
+    ApiCallData,
+    PostOptions
+} from './types';
+
 const NETMARBLE_API_URL = 'https://nbill.netmarble.net/Cash/AutoPaymentPremium/AutoPaymentAdapter.aspx' as const;
 
 export const USERNAME_PLACEHOLDER = 'id를 입력하세요' as const;
@@ -11,12 +16,12 @@ export const KEYS = {
     enter: 'Enter'
 } as const;
 
-export const DEFAULT_API_CALL_DATA = {
+export const DEFAULT_API_CALL_DATA: Omit<ApiCallData, 'id'> = {
     callType: 'findFriend',
     channelCode: 'NM'
 } as const;
 
-export const DEFAULT_POST_OPTIONS = {
+export const DEFAULT_POST_OPTIONS: Omit<PostOptions, 'form'> = {
     url: NETMARBLE_API_URL,
     encoding: null
 } as const;
